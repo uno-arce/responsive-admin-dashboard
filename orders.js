b6294904a95743c374e2495bc5ffc8ce7ebea37d
user <juanmiguel.arce@cvsu.edu.ph>
2023-09-29 18:13:47 +0800

    new responsive web app

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '969967',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Lozonge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+]
